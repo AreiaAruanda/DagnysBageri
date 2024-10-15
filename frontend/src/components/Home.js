b32e29e4db2aa78373ede0d3aa377244daeb3223
AreiaAruanda <144889202+AreiaAruanda@users.noreply.github.com>
2024-10-15 14:32:39 +0200

    added layout elements, background image and card

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import './Home.css'; // Import the CSS file
 
 // Functional component for the Home page
 const Home = () => {
     return (
-        // Main container div with Bootstrap class
-        <div className="container">
-            <div className="row">
-                {/* Centered column for the content */}
-                <div className="col-12 text-center">
-                    {/* Main heading */}
-                    <h1>Mormor Dagnys Bageri</h1>
-                    {/* Description paragraph */}
-                    <p>This is the home page of our webshop</p>
+        <div className="background">
+            <div className="container">
+                <div className="row">
+                    {/* Centered column for the content */}
+                    <div className="col-12">
+                        {/* Main heading */}
+                        <h1>Mormor Dagnys Bageri</h1>
+                        {/* Description paragraph */}
+                        <p>This is the home page of our webshop</p>
+                    </div>
                 </div>
             </div>
         </div>
