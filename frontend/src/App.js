6f102876fedd6830412a05a68c16a8ad7513bc04
Eve88 <117602364+EllenV88@users.noreply.github.com>
2024-10-16 11:48:01 +0200

    Created navbar with links
    Routes to home, contact, products and cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
+// src/App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import necessary components from react-router-dom
 import Home from './components/Home'; // Import the Home component
 import './App.css'; // Import the CSS file for styling
+import Navbar from './components/Navbar'; // Import the Navbar component
+import Products from './components/Products'; // Import the Products component
+import Contact from './components/Contact'; // Import the Contact component
+import Cart from './components/Cart'; // Import the Cart component
 
 // Main App component
 function App() {
@@ -8,11 +13,16 @@ function App() {
     // Router component to enable routing in the application
     <Router>
       <div className="App">
-        {/* Routes component to define the different routes in the application */}
+        <Navbar />
+
+       
         <Routes>
-          {/* Route for the home page, rendering the Home component */}
           <Route exact path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
+        
       </div>
     </Router>
   );
---
+++ frontend/src/components/Cart.js
@@ -0,0 +1,13 @@
+// src/components/Cart.js
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <div className="content">
+      <h1>Your Shopping Cart</h1>
+      <p>Items in your cart will appear here.</p>
+    </div>
+  );
+};
+
+export default Cart;
